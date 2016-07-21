fed44004b01764215956e933005eaf271dd0f3b4
Matthew Wilkinson <Scfix@outlook.com>
2016-07-21 12:24:55 -0400

    Getting angular into the application having it run as a default script.

--- TurnerWebTest/App_Start/BundleConfig.cs
+++ TurnerWebTest/App_Start/BundleConfig.cs
@@ -18,6 +18,7 @@ namespace TurnerWebTest
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
+					  "~/Scripts/angular.js",
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
