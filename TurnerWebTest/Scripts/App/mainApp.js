68698105d7e1d8b5f80d1da55dd7a5a5550d528d
Matthew Wilkinson <scfix@outlook.com>
2016-07-22 00:56:38 -0400

    Added directives and styled the front page a bit.

--- TurnerWebTest/Scripts/App/mainApp.js
+++ TurnerWebTest/Scripts/App/mainApp.js
@@ -16,4 +16,21 @@
 					})
 			}
 		}
+	}])
+	.directive('onKeyEnter', ['$parse', function ($parse) {
+		return {
+			restrict: 'A',
+			link: function (scope, element, attrs) {
+				element.bind('keydown keypress', function (event) {
+					if (event.which === 13) {
+						var attrValue = $parse(attrs.onKeyEnter);
+						(typeof attrValue === 'function') ? attrValue(scope) : angular.noop();
+						event.preventDefault();
+					}
+				});
+				scope.$on('$destroy', function () {
+					element.unbind('keydown keypress')
+				})
+			}
+		};
 	}]);
