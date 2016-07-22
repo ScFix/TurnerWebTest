51df31a3fae3646c2647ac1b387e7eab121f53c1
Matthew Wilkinson <scfix@outlook.com>
2016-07-21 20:24:04 -0400

    Removing stupid extra stuff and added dbcontext

---
+++ TurnerWebTest/Controllers/TitlesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TurnerWebTest.Entities;
+
+namespace TurnerWebTest.Controllers
+{
+	public class TitlesController : ApiController
+	{
+		public IEnumerable<Title> Get(string token)
+		{
+			using (var entities = new TitlesEntities())
+			{
+			}
+			//return tt.ToList();
+			return null;
+		}
+	}
+}
